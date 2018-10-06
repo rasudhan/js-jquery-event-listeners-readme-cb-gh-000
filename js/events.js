1be758aa403ee3a20507d0da42eed4e1a23a5618
Anandha Sudhan <rasudhan@gmail.com>
2018-10-06 23:57:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,12 @@ function getIt() {
   $('p').on('click',alert("Hey!"));
 }
 
+function frameIt() {
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
